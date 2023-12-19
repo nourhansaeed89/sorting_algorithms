c2f6eccf4b6bf78c1a1df373bfd086cfa1bd3b79
nourhan <nourhansaeed89@gmail.com>
2023-12-19 09:32:54 -0800

    soting

--- sort.h
+++ sort.h
@@ -19,6 +19,7 @@ typedef struct listint_s
 	struct listint_s *prev;
 	struct listint_s *next;
 } listint_t;
+void print_array(const int *array, size_t size);
 
 void bubble_sort(int *array, size_t size);
 void insertion_sort_list(listint_t **list);
